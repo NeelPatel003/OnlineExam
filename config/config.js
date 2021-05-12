1b60bb6eb363be847174378a96fe208dbefad86c
Neel <patelneel033@gmail.com>
2021-05-12 13:45:23 +0530

    add postinstall

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  db: "mongodb://localhost/examGround",
+  db:
+    "mongodb+srv://dbUser:dbUser@cluster0.grzp1.mongodb.net/examground?retryWrites=true&w=majority",
   jwtsecret: "97hw9a73hr2q@#$#@mo8afjoeidha0e8doh",
   username: "",
   pass: "",
